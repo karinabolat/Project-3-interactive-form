ab625664fb102bc1c453780367ee4386f165427d
karinabolat <90366213+karinabolat@users.noreply.github.com>
2021-10-08 23:40:50 -0500

    Update code for Exceeds

--- js/script.js
+++ js/script.js
@@ -8,9 +8,7 @@ otherJob.style.display = 'none';
 
 const userTitle = document.getElementById('title');
 userTitle.addEventListener('change', e => {
-    if (e.target.value === 'other') {
-        otherJob.style.display = '';
-    } else otherJob.style.display = 'none';
+    (e.target.value === 'other')? otherJob.style.display = '' : otherJob.style.display = 'none';
 });
 
 //'Color' field is updated depending on 'Design' field
@@ -34,15 +32,35 @@ userDesign.addEventListener('change', e=> {
     }
 });
 
-//Updates total cost depending on user selection
+//Updates total cost depending on user selection as well as disables conflicting time slots.
 const totalAmount = document.getElementById('activities-cost');
 const selectedActivities = document.getElementById('activities-box');
+const inputAll = document.querySelectorAll('input[type="checkbox"]');
 let costAll = 0;
 
 selectedActivities.addEventListener('change', e => {
     const selectedCost = parseInt(e.target.getAttribute('data-cost'));
     (e.target.checked)? costAll += selectedCost : costAll -= selectedCost;
     totalAmount.textContent = `Total: $${costAll}`;
+    
+    const dayTime = e.target.getAttribute('data-day-and-time');
+    const isChecked = e.target.checked;
+
+    if (isChecked) {
+        for (let i=0; i < inputAll.length; i++) {
+            if (e.target !== inputAll[i] && dayTime === inputAll[i].getAttribute('data-day-and-time')) {
+                inputAll[i].disabled = true;
+                inputAll[i].parentElement.classList.add('disabled');
+            }
+        }
+    } else {
+        for (let i=0; i < inputAll.length; i++) {
+            if (dayTime === inputAll[i].getAttribute('data-day-and-time')) {
+                inputAll[i].disabled = false;
+                inputAll[i].parentElement.classList.remove('disabled');
+            }
+        }
+    }
 });
 
 //Makes credit card default option and updates section if user selects other options
@@ -57,18 +75,22 @@ paypal.style.display = 'none';
 
 paymentAll.addEventListener('change', e =>{
     const selectedPayment = e.target.value;
-    if (selectedPayment === 'paypal') {
-        creditCard.style.display = 'none';
-        bitcoin.style.display = 'none';
-        paypal.style.display = '';
-    } else if (selectedPayment === 'credit-card') {
+    switch (selectedPayment) {
+        case 'paypal':
+            creditCard.style.display = 'none';
+            bitcoin.style.display = 'none';
+            paypal.style.display = '';
+            break;
+        case 'credit-card':
             creditCard.style.display = '';
             bitcoin.style.display = 'none';
             paypal.style.display = 'none';
-    } else if (selectedPayment === 'bitcoin') {
-        creditCard.style.display = 'none';
-        bitcoin.style.display = '';
-        paypal.style.display = 'none';
+            break;
+        case 'bitcoin':
+            creditCard.style.display = 'none';
+            bitcoin.style.display = '';
+            paypal.style.display = 'none';
+            break;
     }
 });
 
@@ -87,14 +109,26 @@ function isNameValid() {
     return valid;
 }
 
-//Must be a valid email address and contain "@" and ".com"
+// Real-time error hint
+userName.addEventListener ('keyup', isNameValid);
+
+// Must be a valid email address and contain "@" and ".com". Provides two differenet error messages.
 function isEmailValid() {
-    const valid = /^[^@]+\@[^@.]+\.com$/i.test(email.value);
-    (valid)? hintNotRequired(email) : hintRequired (email);
-    return valid;
+    const isEmpty = /^\s*$/.test(email.value);
+    const isValid = /^[^@]+\@[^@.]+\.com$/i.test(email.value);
+
+    if (isEmpty) {
+        hintRequired(email);
+        email.parentElement.lastElementChild.innerHTML = 'Email field cannot be blank';
+    } else if (!isValid) {
+        hintRequired(email);
+        email.parentElement.lastElementChild.innerHTML = 'Email address must contain @ sign and end with ".com"';
+    } else hintNotRequired(email);
+    
+    return true;
 }
 
-//At least one activity is selected
+// Checks if at least one activity is selected
 function isActivitySelected() {
     const valid = costAll > 0;
     (valid)? hintNotRequired(selectedActivities) : hintRequired (selectedActivities);
@@ -122,36 +156,29 @@ function isCvvValid() {
     return valid;
 }
 
+// Validates the form
 form.addEventListener('submit', e => {
-    
     if (isNameValid()) {
         e.preventDefault();
     } 
-
     if (!isEmailValid()) {
         e.preventDefault();
-    } 
-    
+    }   
     if (!isActivitySelected()) {
         e.preventDefault();
-    }
-    
+    } 
     if (creditCard.style.display === '' && !isCreditCardValid()) {
         e.preventDefault();
-    }
-    
+    } 
     if (creditCard.style.display === '' && !isZipValid()) {
         e.preventDefault();
-    }
-    
+    } 
     if (creditCard.style.display === '' && !isCvvValid()) {
         e.preventDefault();
     }
 });
 
 // Adding focus to activities for accessibility
-const inputAll = document.querySelectorAll('input[type="checkbox"]');
-
 for (let i=0; i < inputAll.length; i++) {
     inputAll[i].addEventListener('focus', e => {e.target.parentElement.classList.add('focus')});
     inputAll[i].addEventListener('blur', e => {e.target.parentElement.classList.remove('focus')});
@@ -163,7 +190,6 @@ function hintRequired (element) {
     element.parentElement.classList.remove('valid');
     element.parentElement.lastElementChild.style.display = 'block';
 }
-
 function hintNotRequired (element) {
     element.parentElement.classList.add('valid');
     element.parentElement.classList.remove('not-valid');
